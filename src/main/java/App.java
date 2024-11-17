99acefdc72a86d52457eca75e6420e2f50f3c178
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-11-18 03:11:13 +0530

    added if condition to checck the data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
 public class App {
     public void addData(String data){
         System.out.println("data: "+data);
+        if(data.equalsIgnoreCase("java")){
+            System.out.println("Hello");
+        }
     }
     public static void main(String[] args){
         System.out.println("Hello world!");
