33e1067ebfeddcac9431bd9bd21019f244dedba0
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-12-12 22:04:54 +0530

    changed file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
     public void addData(String data){
         System.out.println("data: "+data);
         if(data.equalsIgnoreCase(Constant.DATA_TYPE)){
-            System.out.println("Hello");
+            System.out.println("Hello"); 
         }
     }
     public static void main(String[] args){
