a03a5017a9a198311d8f6ce27c99468d7b9b0816
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-12-12 23:08:20 +0530

    code changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,12 @@ public class App {
         System.out.println("data: "+data);
         if(data.equalsIgnoreCase(Constant.DATA_TYPE)){
             System.out.println("Hello");
-        }else if{
+        }else if(true){
             System.out.println("Welcome");
             System.out.println("Display");
         }else{
             System.out.println("Display else block");
+        }
     }
     public static void main(String[] args){
         System.out.println("Hello world!");
