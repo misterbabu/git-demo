d380a966a30abea465cd6485f839174a380436c6
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-12-12 22:32:28 +0530

    commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
             System.out.println("Hello");
         }else {
             System.out.println("Welcome");
+            System.out.println("Display");
         }
     }
     public static void main(String[] args){
