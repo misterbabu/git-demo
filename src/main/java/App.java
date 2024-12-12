c0e1c85c824dedc7c7269cf2d9bc491df071bf23
misterbabu <85179837+misterbabu@users.noreply.github.com>
2024-12-12 22:47:58 +0530

    Update App.java
    committed by developer 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,11 @@ public class App {
         System.out.println("data: "+data);
         if(data.equalsIgnoreCase(Constant.DATA_TYPE)){
             System.out.println("Hello");
-        }else {
+        }else if{
             System.out.println("Welcome");
             System.out.println("Display");
-        }
+        }else{
+            System.out.println("Display else block");
     }
     public static void main(String[] args){
         System.out.println("Hello world!");
